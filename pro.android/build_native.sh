8135ff2a2bd13d7f86c023abcd261447ec414be0
yanli.huang <yanli.huang@chukong-inc.com>
2015-03-11 21:39:47 +0800

    delete pro.android

--- pro.android/build_native.sh
+++
@@ -1,4 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-NDK_ROOT=/Users/daisy/Documents/software/anysdk/adt-bundle-mac-x86_64/android-ndk-r9d
-$NDK_ROOT/ndk-build -C $DIR \
-"NDK_MODULE_PATH=$DIR"
--- pro.android/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_STL := gnustl_static
-NDK_TOOLCHAIN_VERSION=clang
-APP_CPPFLAGS += -frtti
-APP_ABI :=armeabi armeabi-v7a
